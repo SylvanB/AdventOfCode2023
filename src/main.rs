5f87049e84c2b302e5283834cb9931f2047f204e
Sylvan Bowdler <sylvanbowdler18@gmail.com>
2023-12-01 22:01:53 +0000

    Day 1: Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::fs::File;
-use std::io;
 use std::io::{BufRead, BufReader};
-use regex::Regex;
 use clap::Parser;
+use aho_corasick::AhoCorasick;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -26,26 +25,34 @@ fn main() {
 fn day1(path: String) {
     let file = File::open(path).unwrap();
     let buffer = BufReader::new(file);
-    let num_regex = Regex::new("\\d").unwrap();
+    let patterns = [
+        "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
 
-    let total = buffer.lines()
-        .fold(0, |acc, x| {
-            let line = x.unwrap();
-            let nums: Vec<char> = line
-                .chars()
-                .filter(|char| char.is_digit(10))
-                .collect();
+    let ac = AhoCorasick::new(patterns).unwrap();
 
-            let num_str = match nums.len() {
-                0 => { return acc; },
-                1 => format!("{}{}", &nums[0], &nums[0]),
-                n => format!("{}{}", &nums[0], &nums[n-1])
-            };
 
-            let num = num_str.parse::<u32>().unwrap();
-            acc + num
-        });
-
+    let total = buffer.lines().fold(0, |acc, l| acc + parse_calibration_string(&ac, &l.unwrap()));
     println!("Sum of calibration values: {}", total);
 }
 
+fn parse_calibration_string(ac: &AhoCorasick, input: &str ) -> u32 {
+    let matches: Vec<u32> = ac.find_overlapping_iter(input).map(|mat| {
+        let pattern_idx = mat.pattern().as_u32();
+        match pattern_idx {
+            0..=8 => pattern_idx + 1,
+            9..=17 => pattern_idx - 8,
+            _ => 0
+        }
+    }).collect();
+
+    let num = match matches.len() {
+        0 => (0, 0),
+        1 => (matches[0], matches[0]),
+        n => (matches[0], matches[n - 1])
+    };
+
+    num.0 * 10 + num.1
+}
