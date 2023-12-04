5242f132e7003350898b9b5ef5cb74ef72b5d875
Sylvan Bowdler <sylvanbowdler18@gmail.com>
2023-12-04 20:00:55 +0000

    Day 4: Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -6,14 +6,39 @@ pub(crate) fn day4(path: String) {
     let buffer = BufReader::new(file);
     let lines = buffer.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
 
-    let split_lines = lines.iter().map(|l| l.split(&[':', '|']).collect::<Vec<&str>>()).collect::<Vec<Vec<&str>>>();
+    let split_lines = lines
+        .iter()
+        .map(|l| l.split(&[':', '|']).collect::<Vec<&str>>())
 
-    let total_points = split_lines.iter().fold(0, |acc, x| acc + parse_winning_numbers(x));
+        .collect::<Vec<Vec<&str>>>();
+    let winners_for_card = split_lines
+        .iter()
+        .map(|l| get_winning_numbers(l))
+        .collect::<Vec<Vec<u32>>>();
 
-    println!("{}", total_points);
+    let total_points = winners_for_card
+        .iter()
+        .fold(0, |acc, winners| acc + calculate_points_for_card(winners));
+
+    let mut card_count = vec![0_u32; winners_for_card.len()];
+    for (idx, winners) in winners_for_card.iter().enumerate() {
+        card_count[idx] += 1;
+        let is_winner = winners.len() > 0;
+        if is_winner {
+            for _count in 0..card_count[idx] {
+                for n in 0..winners.len() {
+                    card_count[idx + n + 1] += 1;
+                }
+            }
+        }
+    }
+
+    let total_card_count = card_count.into_iter().fold(0, |acc, n| acc + n);
+    println!("Total Points: {}", total_points);
+    println!("Total Count of Cards: {}", total_card_count);
 }
 
-fn parse_winning_numbers(split_line: &Vec<&str>) -> u32{
+fn get_winning_numbers(split_line: &Vec<&str>) -> Vec<u32> {
     let winning = split_line[1]
         .trim()
         .split(" ")
@@ -25,11 +50,13 @@ fn parse_winning_numbers(split_line: &Vec<&str>) -> u32{
         .trim()
         .split(" ")
         .filter(|x| !x.trim().is_empty())
-        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| x.parse::<u32>().unwrap().to_owned())
         .collect::<Vec<u32>>();
 
-    let winners = mine.iter().filter(|x| winning.contains(x)).collect::<Vec<&u32>>();
+    mine.into_iter().filter(|x| winning.contains(x)).collect()
+}
 
+fn calculate_points_for_card(winners: &Vec<u32>) -> u32 {
     match winners.len() {
         0 => 0,
         1 => 1,
@@ -38,5 +65,4 @@ fn parse_winning_numbers(split_line: &Vec<&str>) -> u32{
             2_u32.pow(exp as u32)
         }
     }
-
 }
