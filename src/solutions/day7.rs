5b8f9ee943a46acd2a9e4f94a2fe21abcd6ce232
Sylvan Bowdler <sylvanbowdler18@gmail.com>
2023-12-08 22:25:48 +0000

    Day 7: Part 1: Implemented Tests
    Still broken :(

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -1,18 +1,16 @@
 use std::cmp::Ordering;
-use std::collections::HashMap;
+use std::collections::{BTreeMap};
 use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::{BufReader, Read};
 use std::str::FromStr;
-use std::sync::mpsc::TrySendError::Full;
 use itertools::Itertools;
 use nom::bytes::complete::{is_a, tag};
 use nom::character::complete::{space1, u32};
 use nom::IResult;
 use nom::multi::separated_list1;
 use nom::sequence::tuple;
-use strum_macros::EnumString;
-use crate::solutions::day7::HandType::{FiveOfAKind, HighCard, ThreeOfAKind, TwoPair};
+use strum_macros::{Display, EnumString};
 
 pub fn day7(path: String) {
     let file = File::open(path).unwrap();
@@ -20,61 +18,63 @@ pub fn day7(path: String) {
     let mut i = String::new();
     _ = buffer.read_to_string(&mut i);
 
-    let (i, mut hands) = parse_hands(&i).unwrap();
-    hands.sort();
-    let total_winnings = hands.iter().enumerate().fold(0, |acc, (i,h)| acc + (h.bid * i as u32));
+    // let (i, mut hands) = parse_hands(&i).unwrap();
+    // hands.sort();
+    // let mut total_winnings = 0;
+    // for (idx, h) in hands.iter().enumerate() {
+    //     let a = idx + 1;
+    //     total_winnings += h.bid * a as u32;
+    // }
+    let total_winnings = get_total_winnings(&i);
 
     println!("{}", total_winnings);
 }
 
-/*
-32T3K (3*5 + 2*4 + 10*3 + 3*2 + 13*1) = 72
-T55J5 (10*5 + 5*4 + 5*3 + 11*2 + 5*1) = 112
-KK677 (13*5 + 12*4 + 6*3 + 7*2 + 7*1) = 152
-KTJJT (13*5 + 10*4 + 11*3 + 11*2 + 10*1) = 170
-QQQJA (12*5 + 12*4 + 12*3 + 11*2 + 14*1) = 180
+fn get_total_winnings(i: &str) -> u32 {
+    let (_, mut hands) = parse_hands(&i).unwrap();
+    hands.sort();
+    let mut total_winnings = 0;
+    for (idx, h) in hands.iter().enumerate() {
+        let a = idx + 1;
+        println!("{}", format!("Rank: {} Type: {} Value: {} Raw: {}", idx + 1, &h.hand_type, &h.card_value, &h.raw_hand));
+        total_winnings += h.bid * a as u32;
+    }
 
-1. 32T3K = 72
-2. KTJJT = 170
-3. KK677 = 152
-4. T55J5 = 112
-5. QQQJA = 180
-*/
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, EnumString)]
-#[repr(u32)]
-enum Card {
-    #[strum(serialize = "1")]
-    One = 1,
-    #[strum(serialize = "2")]
-    Two = 2,
-    #[strum(serialize = "3")]
-    Three = 3,
-    #[strum(serialize = "4")]
-    Four = 4,
-    #[strum(serialize = "5")]
-    Five = 5,
-    #[strum(serialize = "6")]
-    Six = 6,
-    #[strum(serialize = "7")]
-    Seven = 7,
-    #[strum(serialize = "8")]
-    Eight = 8,
-    #[strum(serialize = "9")]
-    Nine = 9,
-    #[strum(serialize = "T")]
-    Ten = 10,
-    #[strum(serialize = "J")]
-    Jack = 11,
-    #[strum(serialize = "Q")]
-    Queen = 12,
-    #[strum(serialize = "K")]
-    King = 13,
-    #[strum(serialize = "A")]
-    Ace = 14
+    total_winnings
 }
 
-#[derive(Eq,PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash, EnumString)]
+#[repr(u32)]
+enum Card {
+    #[strum(serialize = "2")]
+    Two     = 2,
+    #[strum(serialize = "3")]
+    Three   = 4,
+    #[strum(serialize = "4")]
+    Four    = 8,
+    #[strum(serialize = "5")]
+    Five    = 16,
+    #[strum(serialize = "6")]
+    Six     = 32,
+    #[strum(serialize = "7")]
+    Seven   = 64,
+    #[strum(serialize = "8")]
+    Eight   = 128,
+    #[strum(serialize = "9")]
+    Nine    = 256,
+    #[strum(serialize = "T")]
+    Ten     = 512,
+    #[strum(serialize = "J")]
+    Jack    = 1024,
+    #[strum(serialize = "Q")]
+    Queen   = 2048,
+    #[strum(serialize = "K")]
+    King    = 4096,
+    #[strum(serialize = "A")]
+    Ace     = 8192
+}
+
+#[derive(Display, Debug, Eq,PartialEq, Ord, PartialOrd, Hash, EnumString)]
 enum HandType {
     FiveOfAKind = 7,
     FourOfAKind = 6,
@@ -85,99 +85,257 @@ enum HandType {
     HighCard = 1
 }
 
-#[derive(Eq)]
-struct Hand {
+#[derive(Debug, Eq)]
+struct Hand<'a> {
+    raw_hand: &'a str,
     card_value: u32,
-    // cards: HashMap<Card, u32>,
     bid: u32,
     hand_type: HandType,
 }
 
-impl PartialEq for Hand {
+impl PartialEq for Hand<'_> {
     fn eq(&self, other: &Self) -> bool {
         self.hand_type == other.hand_type && self.card_value == other.card_value
     }
 
 }
 
-impl Hash for Hand {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.hand_type.hash(state);
-        self.card_value.hash(state);
-    }
-}
+// impl Hash for Hand {
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self.hand_type.hash(state);
+//         self.card_value.hash(state);
+//     }
+// }
 
-impl PartialOrd for Hand {
+impl PartialOrd for Hand<'_> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+        let result = if &self.hand_type.cmp(&other.hand_type) == &Ordering::Equal {
+            self.card_value.cmp(&other.card_value)
+        } else {
+            self.hand_type.cmp(&other.hand_type)
+        };
+
+        Some(result)
     }
 }
 
-impl Ord for Hand {
+impl Ord for Hand<'_> {
     fn cmp(&self, other: &Self) -> Ordering {
-        (&self.hand_type, &self.card_value).cmp(&(&other.hand_type, &other.card_value))
+        if &self.hand_type.cmp(&other.hand_type) == &Ordering::Equal {
+            self.card_value.cmp(&other.card_value)
+        } else {
+            self.hand_type.cmp(&other.hand_type)
+        }
     }
 }
 
-impl Hand {
-    pub fn new(cards: &str, bid: u32) -> Self {
-        let cards= cards
+impl<'Hand> Hand<'Hand> {
+    pub fn new(cards: &'Hand str, bid: u32) -> Hand<'Hand> {
+        let parsed_cards: Vec<Card> = cards
             .split("")
             .filter(|x| !x.is_empty())
             .map(|c| Card::from_str(c).unwrap())
-            .fold(HashMap::new(), |mut acc, c| {
+            .collect();
+
+        let cards_freq = parsed_cards.clone().into_iter()
+            .fold(BTreeMap::new(), |mut acc, c| {
                 *acc.entry(c).or_insert(0) += 1;
                 acc
             });
 
+        let keys = cards_freq.keys().collect::<Vec<&Card>>();
         let mut hand = Self {
-            card_value: cards.iter().fold(0, |acc, (c, n)| acc + ((*c) as u32 * n)),
-            // cards,
+            raw_hand: cards,
+            card_value: Self::calculate_value(parsed_cards),
             bid,
-            hand_type: HighCard
+            hand_type: HandType::HighCard
         };
 
-        hand.hand_type = get_type(&cards);
+        hand.hand_type = get_type(&cards_freq);
 
         hand
     }
 
+    fn calculate_value(cards: Vec<Card>) -> u32 {
+    //     |acc, (idx, (c, count))| {
+    //         acc + (5 - idx as u32) * (*c) as u32 * count
+    //     }
+        let mut score = 0;
+
+        for (idx, card) in cards.into_iter().enumerate() {
+            let card_pos: u32 = 5 - idx as u32;
+            score += card as u32 * card_pos;
+        }
+
+        score
+    }
 }
 
-fn get_type(cards: &HashMap<Card, u32>) -> HandType {
+fn get_type(cards: &BTreeMap<Card, u32>) -> HandType {
     // Sorts the cards incorrectly by Card not count
     let sorted: Vec<(&Card, &u32)> = cards.iter()
-        .sorted_by(|a, b| b.1.cmp(a.1))
+        // TODO: Why does this break everything??
+        // .sorted_by(|a, b| b.1.cmp(a.1))
+
         .collect();
 
     match sorted[0] {
         (_, 5) => {
-            FiveOfAKind
+            HandType::FiveOfAKind
         },
-        (_, 4) => TwoPair,
+        (_, 4) => HandType::FourOfAKind,
         (_, 3) => {
             match sorted[1] {
                 (_, 2) => HandType::FullHouse,
-                _ => ThreeOfAKind
+                _ => HandType::ThreeOfAKind
             }
         },
         (_, 2) => {
             match sorted[1] {
-                (_, 2) => TwoPair,
+                (_, 2) => HandType::TwoPair,
                 _ => HandType::OnePair,
             }
         },
-        (_, 1) => HighCard,
+        (_, 1) => HandType::HighCard,
         _ => { unreachable!("All cards are of some type - at minimum a hand must be a HighCard")}
     }
 }
 
 fn parse_hand(i: &str) -> IResult<&str, Hand> {
-    tuple((is_a("123456789TJQKA"), space1, u32))(i).map(|(i, (cards, _, bid))| (i, Hand::new(cards, bid)))
+    tuple((is_a("23456789TJQKA"), space1, u32))(i).map(|(i, (cards, _, bid))| (i, Hand::new(cards, bid)))
 }
 
 fn parse_hands(i: &str) -> IResult<&str, Vec<Hand>> {
     let (i, hand) = separated_list1(tag("\n"), parse_hand)(i)?;
 
     Ok((i, hand))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_parse_hand_correctly() {
+        assert_eq!(parse_hand("A2345 101").unwrap(), ("", Hand::new("A2345", 101)));
+        assert_eq!(parse_hand("AAKKQ 202").unwrap(), ("", Hand::new("AAKKQ", 202)));
+        assert_eq!(parse_hand("AAKJQ 303").unwrap(), ("", Hand::new("AAKJQ", 303)));
+    }
+
+    #[test]
+    fn should_parse_multiple_hands_correctly() {
+        assert_eq!(parse_hands("A2345 101\nAAKKQ 202\nAAKJQ 303").unwrap(), (
+            "",
+            vec![
+                Hand::new("A2345", 101),
+                Hand::new("AAKKQ", 202),
+                Hand::new("AAKJQ", 303)
+            ]));
+    }
+
+    #[test]
+    fn hand_should_order_by_highest_value_first_occurring_card() {
+        assert_eq!(Hand::new("A2345", 0), Hand::new("A2345", 0));
+        assert!(Hand::new("2A345", 0) < Hand::new("A2345", 0));
+        assert!(Hand::new("A2345", 0) > Hand::new("2A345", 0));
+        assert!(Hand::new("QA234", 0) < Hand::new("QAA34", 0));
+    }
+
+    #[test]
+    fn parses_correct_hand_type() {
+        assert_eq!(Hand::new("A2345", 0).hand_type, HandType::HighCard);
+        assert_eq!(Hand::new("AA345", 0).hand_type, HandType::OnePair);
+        assert_eq!(Hand::new("A345A", 0).hand_type, HandType::OnePair);
+        assert_eq!(Hand::new("AAK5K", 0).hand_type, HandType::TwoPair);
+        assert_eq!(Hand::new("A4AA5", 0).hand_type, HandType::ThreeOfAKind);
+        assert_eq!(Hand::new("AA5AA", 0).hand_type, HandType::FourOfAKind);
+        assert_eq!(Hand::new("AAAAA", 0).hand_type, HandType::FiveOfAKind);
+        assert_eq!(Hand::new("AKAKK", 0).hand_type, HandType::FullHouse);
+    }
+
+    #[test]
+    fn should_order_hands_by_type_then_value() {
+        let hands = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+"#;
+        let (_, mut hands) = parse_hands(hands).unwrap();
+
+        hands.sort();
+        assert_eq!(
+            hands,
+            vec![
+                Hand::new("32T3K", 765),
+                Hand::new("KTJJT", 220),
+                Hand::new("KK677", 28),
+                Hand::new("T55J5", 684),
+                Hand::new("QQQJA", 483)
+            ]
+        );
+    }
+
+    #[test]
+    fn should_calculate_correct_winnings() {
+        let hands = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+"#;
+
+        assert_eq!(get_total_winnings(hands), 6440);
+    }
+
+    #[test]
+    fn should_calculate_correct_winnings2() {
+        let hands = r#"AAAAA 2
+22222 3
+AAAAK 5
+22223 7
+AAAKK 11
+22233 13
+AAAKQ 17
+22234 19
+AAKKQ 23
+22334 29
+AAKQJ 31
+22345 37
+AKQJT 41
+23456 43
+"#;
+        assert_eq!(get_total_winnings(hands), 1343);
+    }
+
+    #[test]
+    fn should_calculate_correct_winnings3() {
+        let hands = r#"23456 22
+56789 19
+KJJKK 2
+AAAAJ 3
+JJ243 7
+QJ256 6
+QQ562 5
+Q8Q24 4
+AAAAT 3
+TJJJJ 2
+6789T 18
+789TJ 17
+22345 13
+34567 21
+45678 20
+32245 12
+33245 11
+89TJQ 16
+9TJQK 15
+TJQKA 14
+3J245 10
+J3425 9
+J5432 8
+JJJJJ 1
+"#;
+        assert_eq!(get_total_winnings(hands), 2237);
+    }
+
 }
